0855d7fda5c85a8278a1fa0ef846925bccfd4ccc
Emily Wong <emilywong99@gmail.com>
2016-05-26 21:41:23 -0700

    IT WORKS YAY
    I’m awesome

--- Space Invaders/javascript/helpers.js
+++ Space Invaders/javascript/helpers.js
@@ -1,6 +1,10 @@
 
 //Helper functions
 
+function intersect(ax, ay, aw, ah, bx, by, bw, bh) {
+	return ax < bx+bw && bx < ax+aw && ay < by+bh && by < ay+ah;
+}
+
 //Bullet
 function Bullet(x, y, vely, w, h, color){
 	this.x = x;
